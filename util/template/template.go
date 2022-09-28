a384090ee0dd1e6218221ee378cfaee3e4b946d6
hopher.hong <565983236@qq.com>
2022-09-28 20:27:35 +0800

    增加注释说明

--- util/template/template.go
+++ util/template/template.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// New 创建模版引擎，加载主题模板文件
 func New(name string, patterns ...string) (*template.Template, error) {
 	// 初始化模板引擎 并加载模板文件
 	htmlTplEngine := template.New(name).Funcs(template.FuncMap{
@@ -28,6 +29,7 @@ func unescapeHTML(s string) template.HTML {
 	return template.HTML(s)
 }
 
+// timeFormat 时间格式化
 func timeFormat(t time.Time) string {
 	return t.Format("2006-01-02 15:04:05")
 }
